124bd14d9e82350eb2d62c02d55679346d0a3d43
Shea <nu@she-a.eu>
2025-04-28 07:02:17 +0300

    basic reader

--- ansi.js
+++ ansi.js
@@ -1,6 +1,6 @@
 ansible = document.getElementById("ansible");
 
-fontSize = 20;
+fontSize = 15;
 offX = 30;
 offY = 50;
 
@@ -27,21 +27,73 @@ function printer(posX, posY, colbg, colfg, oup){
 	obj.style.letterSpacing 	= "-2px";
 
 	obj.style.position 			= "absolute";
-	obj.style.left				= offX+posX*fontSize+"px";
+	obj.style.left				= offX+posX*(fontSize/2)+"px";
 	obj.style.top 				= offY+posY*fontSize+"px";
 	obj.style.color 			= colfg;
 	obj.style.backgroundColor 	= colbg;
 	obj.innerHTML 				= oup;
 	tally += 1;
 }
-
+/*
 i = 0;
 while(i<100){
 	printer(i*20, 10, defbg, deffg, "█▀");
 	i += 1;
 }
-
+*/
 fetch('test.txt')
   .then(response => response.text())
-  .then(text => console.log(text))
+  .then(text => {
+		tokens = text.split("\x1b");
+		i=0;
+		let posX = 0;
+		let posY = 0;
+		let colbg = defbg;
+		let colfg = deffg;
+		while(i<tokens.length){
+			if(!tokens[i]){i++; continue;}
+			else if(tokens[i].includes("[0m")){
+				posX = 0;
+				posY = 0;
+				colbg = defbg;
+				colfg = deffg;
+			}
+			else if(tokens[i][0] == '[' && tokens[i][tokens[i].length-1] == 'H'){
+				posY = tokens[i].substring(1, tokens[i].indexOf(';'));
+				posX = tokens[i].substring(tokens[i].indexOf(';')+1, tokens[i].indexOf('H'));
+				if(posX == 0) posX = 1;
+			}
+			else if(tokens[i].includes("[2J")){
+				ansible.innerHTML = "";
+				tally = 0;
+			}
+			else if(tokens[i][0] == '['){
+				if(tokens[i].startsWith("[38;2;")){
+					tokens[i] = tokens[i].substring(6);
+					colfg = "rgb("+tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+					colfg+= tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+					colfg+= tokens[i].substring(0, tokens[i].indexOf(';'))+')';
+					tokens[i] = tokens[i].substring(tokens[i].indexOf(';'));
+				}
+				if(tokens[i].startsWith(";48;2;")){
+					tokens[i] = tokens[i].substring(6);
+					colbg = "rgb("+tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+					colbg+= tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+					colbg+= tokens[i].substring(0, tokens[i].indexOf('m'))+')';
+					tokens[i] = tokens[i].substring(tokens[i].indexOf('m')+1);
+				}
+				console.log(tokens[i]);
+				printer(posX,posY, colbg, colfg, tokens[i]);
+			}
+			else{
+				console.log("i"+i+": "+tokens[i]);
+			}
+			i++;
+		}
+		//ansible.innerHTML=(text);
+	})
 
